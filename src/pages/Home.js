d7af920018578bd46c3228539d54a8310ee2c0ce
BartOutOfTheBox <bart.verhoeven144@gmail.com>
2022-09-06 16:33:09 +0200

    header & footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,28 @@ import React from "react";
 import Game from "../components/Sudoku";
 import "./Home.css";
 
+function Header(props) {
+  return (
+    <div id="header">
+      <h1>header</h1>
+    </div>
+  );
+}
+
+function Footer(props) {
+  return (
+    <div id="footer">
+      <h1>footer</h1>
+    </div>
+  );
+}
+
 export default function Home(props) {
-  return <Game />;
+  return (
+    <>
+      <Header />
+      <Game />
+      <Footer />
+    </>
+  );
 }
